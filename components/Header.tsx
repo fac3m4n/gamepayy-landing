a320b6324c1fb24f701e5e97e21778381bab7ff7
Kerim <kerimberdi.agayev@edusoftech.eu>
2023-01-17 23:33:32 +0200

    discord link

--- components/Header.tsx
+++ components/Header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
           <a href="https://twitter.com/Gamepayy">
             <Image src={twitter} alt="twitter" width={32} />
           </a>
-          <a href="https://discord.gg/syz8Ghj7">
+          <a href="https://discord.gg/wZP6hC8W">
             <Image src={discord} alt="discord" width={32} />
           </a>
         </div>
